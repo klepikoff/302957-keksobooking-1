4d6c0541d96af607d892f3070e0f0251132448cd
Nikita <nikita.klepikoff@users.noreply.github.com>
2018-02-25 23:42:12 +0500

    module4-task2 v0

--- js/map.js
+++ js/map.js
@@ -192,3 +192,58 @@ parentPin.addEventListener('click', function (evt) {
     }
   }
 });
+
+// module4-task2
+var appartmentPrice = document.getElementById('price');
+
+var selectType = document.getElementById('type');
+
+// var selectRoomNumber = document.getElementById('room_number');
+
+// 2.3 Поле «Тип жилья» влияет на минимальное значение поля «Цена за ночь»
+
+selectType.addEventListener('change', function () {
+  var selectedType = selectType.options[selectType.selectedIndex].value;
+
+  var appartmentPriceMin = 0;
+
+  if (selectedType === 'flat') {
+    appartmentPriceMin = 1000;
+  }
+  if (selectedType === 'bungalo') {
+    appartmentPriceMin = 0;
+  }
+  if (selectedType === 'house') {
+    appartmentPriceMin = 5000;
+  }
+  if (selectedType === 'palace') {
+    appartmentPriceMin = 10000;
+  }
+  appartmentPrice.setAttribute('min', appartmentPriceMin);
+});
+
+// Поля «Время заезда» и «Время выезда», при изменении значения одного поля, во втором выделяется соответствующее ему.
+
+document.getElementById('timein').addEventListener('change', function () {
+  var selectTimeIn = document.getElementById('timein');
+  var selectedTimeIn = selectTimeIn.options[selectTimeIn.selectedIndex].value;
+
+  var forRemoveOut = document.getElementById('timeout').querySelector('option[selected]');
+  forRemoveOut.removeAttribute('selected');
+
+  var addSelectedOut = document.getElementById('timeout').querySelector('option[value="' + selectedTimeIn + '"]');
+  addSelectedOut.setAttribute('selected', 'selected');
+});
+
+document.getElementById('timeout').addEventListener('change', function () {
+  var selectTimeOut = document.getElementById('timeout');
+  var selectedTimeOut = selectTimeOut.options[selectTimeOut.selectedIndex].value;
+
+  var forRemoveIn = document.getElementById('timein').querySelector('option[selected]');
+  forRemoveIn.removeAttribute('selected');
+
+  var addSelectedIn = document.getElementById('timein').querySelector('option[value="' + selectedTimeOut + '"]');
+  addSelectedIn.setAttribute('selected', 'selected');
+});
+
+// console.log(selectRoomNumber.validity)
