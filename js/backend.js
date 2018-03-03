10ecfa6ec11cb294474356874659f5516181ecce
Nikita <nikita.klepikoff@users.noreply.github.com>
2018-03-03 23:21:47 +0500

    module6-task2 v2

--- js/backend.js
+++ js/backend.js
@@ -1,39 +1,28 @@
 'use strict';
 
-/*
-Создайте модуль backend.js, который экспортирует в глобальную область видимости функции для взаимодействия с удалённым севером через XHR.
-
-- получать с сервера данные с помощью объекта XMLHttpRequest, обрабатывать полученные запросы и передавать полученную информацию в функцию обратного вызова;
-отправлять данные на сервер.
-Функция получения данных с сервера должна принимать на вход следующие параметры:
-
-onLoad — функция обратного вызова, которая срабатывает при успешном выполнении запроса. При вызове функции onLoad в её единственный параметр передаётся набор полученных данных;
-onError — функция обратного вызова, которая срабатывает при неуспешном выполнении запроса. При вызове функции onError в её единственный параметр передаётся сообщение об ошибке.
-
-*/
 (function () {
   window.backend = {};
   var URL = 'https://js.dump.academy/keksobooking/data';
 
-  window.backend.upload = function (data, onSuccess) {
+  window.backend.upload = function (data, onLoad) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      onSuccess(xhr.response);
+      onLoad(xhr.response);
     });
 
     xhr.open('POST', URL);
     xhr.send(data);
   };
 
-  window.backend.load = function (onSuccess, onError) {
+  window.backend.load = function (onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
-        onSuccess(xhr.response);
+        onLoad(xhr.response);
       } else {
         onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
